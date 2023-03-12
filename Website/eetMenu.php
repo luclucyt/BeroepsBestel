57f693a39299c232979cc00937b389ccec5b307e
Pwalhout03 <91482789+Pwalhout03@users.noreply.github.com>
2023-03-13 00:02:36 +0100

    eet menu pagina

--- Website/eetMenu.php
+++ Website/eetMenu.php
@@ -12,6 +12,7 @@
     
     <!-- CSS -->
     <link rel="stylesheet" href="CSS/_menu/menu.css">
+    <link rel="stylesheet" href="CSS/eetMenu/index.css">
     
     <!-- JS -->
     <script src="JS/_menu/menu.js"></script>
@@ -24,8 +25,109 @@
     </header>
 
     <main>
-        <!-- Pauline -->
-        <!-- Pagina hierin AUB -->
+        <div>
+            <img id="menu-img" src="./images/eetMenu/menu.jpeg" alt="menu">
+        </div>
+        <div id="notif-banner">
+            <p>Krijgt u er honger van? Bestel dan <a href="bestel.php">hier </a> online of kom langs bij een van onze filialen</p>
+        </div>
+        <div id="restaurants">
+            <h1>RESTAURANTS</h1>
+            <div id="eet-restaurants">
+                <div id="restaurant-locaties-1">
+                    <p>Almere</p>
+                    <p>Aplhen aan den Rijn</p>
+                    <p>Amesfoort</p>
+                    <p>Amsterdam-Noord</p>
+                    <p>Apeldoorn</p>
+                    <p>Arnhem</p>
+                    <p>Barendrecht</p>
+                    <p>Bleiswijk</p>
+                    <p>Breda</p>
+                    <p>Capelle-Zandrak</p>
+                    <p>De Beren Boot</p>
+                    <p>Delft-Beestenmarkt</p>
+                    <p>Den Bosch</p>
+                    <p>Den Hoorn</p>
+                    <p>Doetichem</p>
+                    <p>Dordrecht - Centrum</p>
+                    <p>Drachten</p>
+                    <p>Ede</p>
+                    <p>Emmen</p>
+                    <p>Goes</p>
+                    <p>Gorichem</p>
+                    <p>Gouda</p>
+                    <p>Groningen</p>
+                    <p>Haarlem</p>
+                    <p>Heenvliet</p>
+                    <p>Heerhugowaard</p>
+                </div>
+                <div id="restaurant-locaties-2">
+                    <p>Hoofddorp</p>
+                    <p>Hoogeveen</p>
+                    <p>Hoorn</p>
+                    <p>Houten</p>
+                    <p>Kerkrade</p>
+                    <p>Leerdam</p>
+                    <p>Maassluis</p>
+                    <p>Meppel</p>
+                    <p>Oosterhout - Markt</p>
+                    <p>Oud-Beijerland</p>
+                    <p>Roosendaal</p>
+                    <p>Rotterdam Alexandrium</p>
+                    <p>Rotterdam Centrum</p>
+                    <p>Rotterdam Markthal</p>
+                    <p>Rotterdam-Zuid</p>
+                    <p>Schiedam-Schieveste</p>
+                    <p>Tilburg</p>
+                    <p>Valkenburg</p>
+                    <p>Veenendaal</p>
+                    <p>Woerden</p>
+                    <p>Zoetemeer - Noord Aa</p>
+                    <p>Zutphen</p>
+                    <p>Zwijndrecht - Develsingel</p>
+                    <p>Zwolle</p>
+                </div>
+            </div>
+            <div id="bezorg-restaurants">
+                <h1>BEZORG RESTAURANTS</h1>
+                <p>Alkmaar</p>
+                <p>Almere Buiten</p>
+                <p>Alphen aan den Rijn</p>
+                <p>Amesfoort-Zuid</p>
+                <p>Apeldoorn-West</p>
+                <p>Barendrecht</p>
+                <p>Berkel en Rodenrijs</p>
+                <p>Beverwijk</p>
+                <p>Breda Haagdijk (GESLOTEN)</p>
+                <p>Delft</p>
+                <p>Den Haag - Scheveningen</p>
+                <p>Dordrecht - Zuid</p>
+                <p>Gouda - Blekerssingel</p>
+                <p>Groningen</p>
+                <p>Heerhugowaard</p>
+                <p>Hengelo</p>
+                <p>Katwijk</p>
+                <p>Leiden-Zuid</p>
+                <p>Leidsche Rijn</p>
+                <p>Lelystad</p>
+                <p>Maassluis</p>
+                <p>Oosterhout - Bredaseweg</p>
+                <p>Oud-Beijerland</p>
+                <p>Papendrecht</p>
+                <p>Ridderkerk</p>
+                <p>Rijswijk</p>
+                <p>Rotterdam Alexandrium Kleine Beer</p>
+                <p>Rotterdam-Noord</p>
+                <p>Rotterdam-Zuid</p>
+                <p>Spijkenisse - Hoogvliet</p>
+                <p>Vlaardinge - Schiedam</p>
+                <p>Voorburg</p>
+                <p>Woerden</p>
+                <p>Zoetemeer</p>
+                <p>Zwijndrecht</p>
+            </div>
+        </div>
     </main>
 
     <footer>
