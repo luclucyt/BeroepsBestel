98c95c8541cfde568f8fb316656eda390b0f8296
Lumenlum <115624862+Lumenlum@users.noreply.github.com>
2023-03-20 09:41:53 +0100

    done voor nu

--- Website/bestel.php
+++ Website/bestel.php
@@ -28,7 +28,7 @@
     <main>
             <div class="item2">
                 <div class="content">
-                    <img class="eten" src="images/bestel/placeholder.png" alt="">
+                    <img class="eten" src="images/bestel/sate-1.avif" alt="">
                     <div class="textWrapper">
                         <p class="tekst1"><strong>SATÉDAGEN SATÉMENU</strong><br>
 2 stokjes kip-, varkenshaas- of vegetarische saté met pindasaus, gefrituurde uitjes, kroepoek en bijgerecht naar keuze
@@ -43,52 +43,48 @@
                 </div>
             <div class="item2">
                 <div class="content">
-                    <img class="eten" src="images/bestel/placeholder.png" alt="">
+                    <img class="eten" src="images/bestel/burger.avif" alt="">
                     <div class="textWrapper">
                         <p class="tekst1"><strong>CLASSIC BURGERMENU</strong><br> 100% rundvleesburger op een brioche bun met Little Gem en onze befaamde Berensaus</p>
                         <p class="tekst2">
-                            <div class="prijs">€ 13,95
+                            <div class="prijs">€ 13,95</div>
                             <div class="add">add to cart</div> 
-                    </div>
                 </div>
             <div class="item2">
                 <div class="content">
-                    <img class="eten" src="images/bestel/placeholder.png" alt="">
+                    <img class="eten" src="images/bestel/carpacio.avif" alt="">
                     <div class="textWrapper">
                         <p class="tekst1"><strong>CARPACCIO
 </strong><br>Met Grana Padano, pijnboompitten, uitgebakken spekjes, gemengde sla en truffelmayo</p>
                         <p class="tekst2">
-                            <div class="prijs">€ 9,95
+                            <div class="prijs">€ 9,95</div>
                             <div class="add">add to cart</div> 
-                    </div>
                 </div>
             <div class="item2">
                 <div class="content">
-                    <img class="eten" src="images/bestel/placeholder.png" alt="">
+                    <img class="eten" src="images/bestel/fish.avif" alt="">
                     <div class="textWrapper">
                         <p class="tekst1"><strong>FISH & CHIPS VEGAN</strong><br>
                         Vegan leckerbeck met plantaardige kruidensaus, rauwkost en ambachtelijke frites
                         </p>
                         <p class="tekst2">
-                            <div class="prijs">€ 16,95
+                            <div class="prijs">€ 16,95</div> 
                             <div class="add">add to cart</div> 
-                    </div>
                 </div>
             <div class="item2">
                 <div class="content">
-                    <img class="eten" src="images/bestel/placeholder.png" alt="">
+                    <img class="eten" src="images/bestel/loaded.avif" alt="">
                     <div class="textWrapper">
                         <p class="tekst1"><strong>LOADED FRIES FRIKANDEL SPECIAAL</strong><br>
                         Franse frites met frikandel, fritessaus, curry, rode ui, Spaanse peper en bieslook
                         </p>
                         <p class="tekst2">
-                            <div class="prijs">€ 9,95
+                            <div class="prijs">€ 9,95</div> 
                             <div class="add">add to cart</div> 
-                    </div>
                 </div>
             <div class="item2">
                 <div class="content">
-                    <img class="eten" src="images/bestel/placeholder.png" alt="">
+                    <img class="eten" src="images/bestel/kinder.avif" alt="">
                     <div class="textWrapper">
                         <p class="tekst1"><strong>COMBIDEAL KIDS</strong><br>
                         Kroket, frikandel, kipnuggets, kipsaté, vegan saté of een kipschnitzel met frites naar keuze, fritessaus, appelmoes
@@ -96,52 +92,50 @@
 + een Berenbeker met ijsje naar keuze
                         </p>
                         <p class="tekst2">
-                            <div class="prijs">€ 10,50
+                            <div class="prijs">€ 10,50</div> 
                             <div class="add">add to cart</div> 
-                    </div>
                 </div>
             <div class="item2">
                 <div class="content">
-                    <img class="eten" src="images/bestel/placeholder.png" alt="">
+                    <img class="eten" src="images/bestel/sate-2.avif" alt="">
                     <div class="textWrapper">
                         <p class="tekst1"><strong>SATÉ BOX</strong><br>
                         Box met 20 of 40 stokjes kipsaté incl pindasaus en gefrituurde uitjes</p>
                         <p class="tekst2">
-                            <div class="prijs">€ 49,95
+                            <div class="prijs">€ 49,95</div> 
                             <div class="add">add to cart</div> 
-                    </div>
                 </div>
             <div class="item2">
                 <div class="content">
-                    <img class="eten" src="images/bestel/placeholder.png" alt="">
+                    <img class="eten" src="images/bestel/biterbal.avif" alt="">
                     <div class="textWrapper">
                         <p class="tekst1"><strong>BITTERBALLEN</strong><br>
                         Bitterballen. Inclusief fritessaus en mosterd. Vanaf 12st
                         </p>
                         <p class="tekst2">
-                            <div class="prijs">€ 8,95
+                            <div class="prijs">€ 8,95</div> 
                             <div class="add">add to cart</div> 
                     </div>
                 </div>
             <div class="item2">
                 <div class="content">
-                    <img class="eten" src="images/bestel/placeholder.png" alt="">
+                    <img class="eten" src="images/bestel/brownie.avif" alt="">
                     <div class="textWrapper">
                         <p class="tekst1"><strong>FUNKY NUTS BROWNIE BAR</strong><br>
                         Smeuïge chocolade notencake met stukjes pure en melkchocolade
                         </p>
                         <p class="tekst2">
-                            <div class="prijs">€ 2,50
+                            <div class="prijs">€ 2,50</div> 
                             <div class="add">add to cart</div> 
                     </div>
                 </div>
             <div class="item2">
                 <div class="content">
-                    <img class="eten" src="images/bestel/placeholder.png" alt="">
+                    <img class="eten" src="images/bestel/six pack.avif" alt="">
                     <div class="textWrapper">
                         <p class="tekst1"><strong>HEINEKEN 6-PACK</strong><br>6x 0,33L</p>
                         <p class="tekst2">
-                            <div class="prijs">€ 11,25
+                            <div class="prijs">€ 11,25</div> 
                             <div class="add">add to cart</div> 
                     </div>
                 </div>
