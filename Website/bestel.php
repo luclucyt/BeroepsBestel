0ec9e99824b7fc169569b99173bc88f620cecb3e
luclucyt <88875@glr.nl>
2023-03-26 09:13:14 +0200

    redo betel
    censent van laim gekregen om de betel pagina logicher op te bouwen
    
    zelfde stijl bewaard

--- Website/bestel.php
+++ Website/bestel.php
@@ -26,134 +26,187 @@
     </header>
 
     <main>
-        <div class="cart_window">
-            <div class="cartWrapper">
-                <div class="wrap_content">
-                    <img class="img" src="images/bestel/sate-1.avif" alt="">
-                    <h1 class="text">satedagen satemenu <input type="number" name="hoeveel" min="1" ></h1><h1 class="pp">€ 9,95 </h1>
+        <div class="main-product-wrapper">
+
+            <div class="product-wrapper">
+                <img class="product-foto" src="images/bestel/sate-1.avif" alt="">
+                <div class="tekst-wrapper">
+                    <h1 class="product-titel">SATÉDAGEN SATÉMENU</h1>
+
+                    <p class="product-omschrijving">
+                        2 stokjes kip-, varkenshaas- of vegetarische saté met pindasaus, gefrituurde uitjes, kroepoek en bijgerecht naar keuze
+                    </p>
+
+                    <div class="product-buy-wrapper">
+                        <p class="product-prijs">€9,95 per stuk</p>
+                        <button class="product-addButton">BESTEL</button>
+                    </div>
                 </div>
             </div>
 
-            <div class="totaal">
-                <h1 id="prijs_totaal">Totaal prijs:</h1>
+
+            <div class="product-wrapper">
+                <img class="product-foto" src="images/bestel/burger.avif" alt="">
+                <div class="tekst-wrapper">
+                    <h1 class="product-titel">CLASSIC BURGERMENU</h1>
+
+                    <p class="product-omschrijving">
+                        100% rundvleesburger op een brioche bun met Little Gem en onze befaamde Berensaus
+                    </p>
+
+                    <div class="product-buy-wrapper">
+                        <p class="product-prijs">€13,95 per stuk</p>
+                        <button class="product-addButton">BESTEL</button>
+                    </div>
+                </div>
             </div>
-            <div id="btn_bestel">
-                <button class="bestel">betalen</button>
+
+            <div class="product-wrapper">
+                <img class="product-foto" src="images/bestel/carpacio.avif" alt="">
+                <div class="tekst-wrapper">
+                    <h1 class="product-titel">CARPACCIO</h1>
+
+                    <p class="product-omschrijving">
+                        Met Grana Padano, pijnboompitten, uitgebakken spekjes, gemengde sla en truffelmayo
+                    </p>
+
+                    <div class="product-buy-wrapper">
+                        <p class="product-prijs">€9,95 per stuk</p>
+                        <button class="product-addButton">BESTEL</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-wrapper">
+                <img class="product-foto" src="images/bestel/fish.avif" alt="">
+                <div class="tekst-wrapper">
+                    <h1 class="product-titel">FISH & CHIPS VEGAN</h1>
+
+                    <p class="product-omschrijving">
+                        Vegan lekerbeck met plantaardige kruidensaus, rauwkost en ambachtelijke frites
+                    </p>
+
+                    <div class="product-buy-wrapper">
+                        <div class="product-prijs">€16,95 per stuk</div>
+                        <button class="product-addButton">BESTEL</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-wrapper">
+                <img class="product-foto" src="images/bestel/loaded.avif" alt="">
+                <div class="tekst-wrapper">
+                    <h1 class="product-titel">LOADED FRIES FRIKANDEL SPECIAAL</h1>
+
+                    <p class="product-omschrijving">
+                        Franse frites met frikandel, fritessaus, curry, rode ui, Spaanse peper en bieslook
+                    </p>
+
+                    <div class="product-buy-wrapper">
+                        <div class="product-prijs">€9,95 per stuk</div>
+                        <button class="product-addButton">BESTEL</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-wrapper">
+                <img class="product-foto" src="images/bestel/kinder.avif" alt="">
+                <div class="tekst-wrapper">
+                    <h1 class="product-titel">COMBIDEAL KIDS</h1>
+
+                    <p class="product-omschrijving">
+                        Kroket, frikandel, kipnuggets, kipsaté, vegan saté of een kipschnitzel met frites naar keuze, fritessaus, appelmoes + een Berenbeker met ijsje naar keuze
+                    </p>
+
+                    <div class="product-buy-wrapper">
+                        <div class="product-prijs">€10,50 per stuk</div>
+                        <button class="product-addButton">BESTEL</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-wrapper">
+                <img class="product-foto" src="images/bestel/sate-2.avif" alt="">
+                <div class="tekst-wrapper">
+                    <h1 class="product-titel">SATÉ BOX</h1>
+
+                    <p class="product-omschrijving">
+                        Box met 20 of 40 stokjes kipsaté incl pindasaus en gefrituurde uitjes
+                    </p>
+
+                    <div class="product-buy-wrapper">
+                        <div class="product-prijs">€49,95 per stuk</div>
+                        <button class="product-addButton">BESTEL</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-wrapper">
+                <img class="product-foto" src="images/bestel/biterbal.avif" alt="">
+                <div class="tekst-wrapper">
+                    <h1 class="product-titel">BITTERBALLEN</h1>
+
+                    <p class="product-omschrijving">
+                        Bitterballen. Inclusief fritessaus en mosterd. Vanaf 12st
+                    </p>
+
+                    <div class="product-buy-wrapper">
+                        <div class="product-prijs">€8,95 per stuk</div>
+                        <button class="product-addButton">BESTEL</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-wrapper">
+                <img class="product-foto" src="images/bestel/brownie.avif" alt="">
+                <div class="tekst-wrapper">
+                    <h1 class="product-titel">FUNKY NUTS BROWNIE BAR</h1>
+
+                    <p class="product-omschrijving">
+                        Smeuïge chocolade notencake met stukjes pure en melkchocolade
+                    </p>
+
+                    <div class="product-buy-wrapper">
+                        <div class="product-prijs">€2,50 per stuk</div>
+                        <button class="product-addButton">BESTEL</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-wrapper">
+                <img class="product-foto" src="images/bestel/six pack.avif" alt="">
+                <div class="tekst-wrapper">
+                    <h1 class="product-titel">HEINEKEN 6-PACK</h1>
+
+                    <p class="product-omschrijving">
+                        6x 0,33L
+                    </p>
+
+                    <div class="product-buy-wrapper">
+                        <div class="product-prijs">€11,25 per stuk</div>
+                        <button class="product-addButton">BESTEL</button>
+                    </div>
+                </div>
             </div>
         </div>
 
-            <div class="item2">
-                <div class="content">
-                    <img class="eten" src="images/bestel/sate-1.avif" alt="">
-                    <div class="textWrapper">
-                        <p class="tekst1"><strong>SATÉDAGEN SATÉMENU</strong><br>
-2 stokjes kip-, varkenshaas- of vegetarische saté met pindasaus, gefrituurde uitjes, kroepoek en bijgerecht naar keuze
-                        </p>
 
-                        <p class="tekst2">
-                            <div class="prijs">€ 9,95</div>
-                            <button class="add">add to cart</button>
-                        </p>
-                    </div>
-                </div>
-            <div class="item2">
-                <div class="content">
-                    <img class="eten" src="images/bestel/burger.avif" alt="">
-                    <div class="textWrapper">
-                        <p class="tekst1"><strong>CLASSIC BURGERMENU</strong><br> 100% rundvleesburger op een brioche bun met Little Gem en onze befaamde Berensaus</p>
-                        <p class="tekst2">
-                            <div class="prijs">€ 13,95</div>
-                            <button class="add">add to cart</button>
-                </div>
-            <div class="item2">
-                <div class="content">
-                    <img class="eten" src="images/bestel/carpacio.avif" alt="">
-                    <div class="textWrapper">
-                        <p class="tekst1"><strong>CARPACCIO
-</strong><br>Met Grana Padano, pijnboompitten, uitgebakken spekjes, gemengde sla en truffelmayo</p>
-                        <p class="tekst2">
-                            <div class="prijs">€ 9,95</div>
-                            <button class="add">add to cart</button>
-                </div>
-            <div class="item2">
-                <div class="content">
-                    <img class="eten" src="images/bestel/fish.avif" alt="">
-                    <div class="textWrapper">
-                        <p class="tekst1"><strong>FISH & CHIPS VEGAN</strong><br>
-                        Vegan leckerbeck met plantaardige kruidensaus, rauwkost en ambachtelijke frites
-                        </p>
-                        <p class="tekst2">
-                            <div class="prijs">€ 16,95</div> 
-                            <button class="add">add to cart</button>
-                </div>
-            <div class="item2">
-                <div class="content">
-                    <img class="eten" src="images/bestel/loaded.avif" alt="">
-                    <div class="textWrapper">
-                        <p class="tekst1"><strong>LOADED FRIES FRIKANDEL SPECIAAL</strong><br>
-                        Franse frites met frikandel, fritessaus, curry, rode ui, Spaanse peper en bieslook
-                        </p>
-                        <p class="tekst2">
-                            <div class="prijs">€ 9,95</div> 
-                            <button class="add">add to cart</button>
-                </div>
-            <div class="item2">
-                <div class="content">
-                    <img class="eten" src="images/bestel/kinder.avif" alt="">
-                    <div class="textWrapper">
-                        <p class="tekst1"><strong>COMBIDEAL KIDS</strong><br>
-                        Kroket, frikandel, kipnuggets, kipsaté, vegan saté of een kipschnitzel met frites naar keuze, fritessaus, appelmoes
+        <div class="cart_window"> test
+            <!--  <div class="cartWrapper">
+                  <div class="wrap_content">
+                      <img class="img" src="images/bestel/sate-1.avif" alt="">
+                      <h1 class="text">satedagen satemenu <input type="number" name="hoeveel" min="1" ></h1><h1 class="pp">€ 9,95 </h1>
+                  </div>
+              </div>
 
-+ een Berenbeker met ijsje naar keuze
-                        </p>
-                        <p class="tekst2">
-                            <div class="prijs">€ 10,50</div> 
-                            <button class="add">add to cart</button>
-                </div>
-            <div class="item2">
-                <div class="content">
-                    <img class="eten" src="images/bestel/sate-2.avif" alt="">
-                    <div class="textWrapper">
-                        <p class="tekst1"><strong>SATÉ BOX</strong><br>
-                        Box met 20 of 40 stokjes kipsaté incl pindasaus en gefrituurde uitjes</p>
-                        <p class="tekst2">
-                            <div class="prijs">€ 49,95</div> 
-                            <button class="add">add to cart</button>
-                </div>
-            <div class="item2">
-                <div class="content">
-                    <img class="eten" src="images/bestel/biterbal.avif" alt="">
-                    <div class="textWrapper">
-                        <p class="tekst1"><strong>BITTERBALLEN</strong><br>
-                        Bitterballen. Inclusief fritessaus en mosterd. Vanaf 12st
-                        </p>
-                        <p class="tekst2">
-                            <div class="prijs">€ 8,95</div> 
-                            <button class="add">add to cart</button>
-                    </div>
-                </div>
-            <div class="item2">
-                <div class="content">
-                    <img class="eten" src="images/bestel/brownie.avif" alt="">
-                    <div class="textWrapper">
-                        <p class="tekst1"><strong>FUNKY NUTS BROWNIE BAR</strong><br>
-                        Smeuïge chocolade notencake met stukjes pure en melkchocolade
-                        </p>
-                        <p class="tekst2">
-                            <div class="prijs">€ 2,50</div> 
-                            <button class="add">add to cart</button>
-                    </div>
-                </div>
-            <div class="item2">
-                <div class="content">
-                    <img class="eten" src="images/bestel/six pack.avif" alt="">
-                    <div class="textWrapper">
-                        <p class="tekst1"><strong>HEINEKEN 6-PACK</strong><br>6x 0,33L</p>
-                        <p class="tekst2">
-                            <div class="prijs">€ 11,25</div> 
-                            <button class="add">add to cart</button>
-                    </div>
-                </div>
+              <div class="totaal">
+                  <h1 id="prijs_totaal">Totaal prijs:</h1>
+              </div>
+              <div id="btn_bestel">
+                  <button class="bestel">betalen</button>
+              </div>-->
+        </div>
+
     </main>
 
     <footer>
