493d33429cf23041db63d5b3c702f39e8ef7140b
Pwalhout03 <91482789+Pwalhout03@users.noreply.github.com>
2023-03-29 12:29:47 +0200

    informatie pagina -> not responsive

--- Website/informatie.php
+++ Website/informatie.php
@@ -24,18 +24,45 @@
     <header>
         <?php include '_menu.php'; ?>
     </header>
-
     <main>
         <div id="background-color">.</div>
-        <img src="images/informatie/header.png" alt="website header">
+        <img id="header-img" src="./images/informatie/header.png" alt="website header">
         <div id="header">
             <h1>ROTTERDAM CENTRUM</h1>
             <p>Restaurant De Beren Rotterdam-centrum is te vinden aan het Schouwburgplein in hartje Rotterdam. Dit maakt het de ideale locatie om te lunchen tijdens een dagje shoppen of te dineren voordat je naar de bioscoop gaat. In de zomermaanden kun je genieten van onze gerechten op het grote en gezellige terras.</p>
         </div>
+        <div id="contact">
+            <h3>Contact:</h3>
+            <p>Karel Doormanstraat</p>
+            <p>3012 GP / Rotterdam</p>
+        </div>
+        <div id="cards">
+            <div class="card">
+                <img src="./images/informatie/openingstijden.jpg" alt="openingstijden img">
+                <h4>Openingstijden</h4>
+                <p>Maandag: 11:00 - 22:00</p>
+                <p>Dinsdag: 11:00 - 22:00</p>
+                <p>Woensdag: 11:00 - 22:00</p>
+                <p>Donderdag: 11:00 - 22:00</p>
+                <p>Vrijdag: 11:00 - 23:30</p>
+                <p>Zaterdag: 11:00 - 23:30</p>
+                <p>Zondag: 11:00 - 22:00</p>
+            </div>
+            <div class="card" id="reserveer-card" onclick=reserveerCard()>
+                <img src="./images/informatie/groepsreservering.jpg" alt="reserving img">
+                <h4>kom je met een groep?</h4>
+                <p>Restaurant de Beren Rotterdam Centraal is uitermate geschikt voor grote gezelschappen vanaf 10 personen. Bel voor meer informatie met 06 29 27 92 12 (kantooruren) of vul het aanvraagformulier in!</p>
+                <p id="aanvraag-txt">Doe hier je aanvraag!</p>
+            </div>  
+            <div>
+                <h1 id="missie">"ALS JIJ ONS RESTAURANT MET EEN GLIMLACH VERLAAT, IS ONZE MISSIE GESLAAGD"</h1>
+                <p id="signature">RICO, VESTIGINGSMANAGER DE BEREN ROTTERDAM-CENTRUM</p>
+            </div>
+        </div>
     </main>
-
     <footer>
         <?php //include '_footer.php'; ?>
     </footer>
 </body>
+<script src="./JS/informatie/index.js"></script>
 </html>
